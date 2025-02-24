cedaf3ff10ab706c35c9346049bcdfd2631c3307
Abdelrazek Emad <53626803+AbdelrazekEmad@users.noreply.github.com>
2025-02-24 20:43:42 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function goTop() {
+  var btn = $("#goTop");
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      "300"
+    );
+  });
+}
+
+AOS.init();
+goTop();
