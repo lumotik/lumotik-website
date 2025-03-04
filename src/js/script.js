152968f3ef623b1809c149c6d6bbe35da0ce5069
Abdelrazek Emad <53626803+AbdelrazekEmad@users.noreply.github.com>
2025-03-05 00:59:14 +0200

    init header and footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,43 @@
-AOS.init();
+function goTop() {
+  var btn = $("#goTop");
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass("show");
+    } else {
+      btn.removeClass("show");
+    }
+  });
+  btn.on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      "300"
+    );
+  });
+}
+
+function closeNav() {
+  $(".navbar-nav.collapsed-mobile").removeClass("active");
+}
+
+$(document).ready(function () {
+  AOS.init();
+  $("#onePageNav").onePageNav({
+    currentClass: "active",
+    changeHash: false,
+    scrollSpeed: 500,
+    scrollThreshold: 0.2,
+    filter: "",
+    easing: "swing",
+  });
+  goTop();
+
+  $(".navbar-toggler").click(function () {
+    $(".navbar-nav.collapsed-mobile").addClass("active");
+  });
+
+  $(".navbar-nav.collapsed-mobile .close__btn").click(closeNav);
+  $(".navbar .overlay").click(closeNav);
+});
