bd34016ddaff183402ce60520fc0e8f74a965a18
Omar Fareed <fareedomar159@gmail.com>
2026-09-05 02:14:13 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -9,12 +9,10 @@ function goTop() {
   });
   btn.on("click", function (e) {
     e.preventDefault();
-    $("html, body").animate(
-      {
-        scrollTop: 0,
-      },
-      "300"
-    );
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 }
 
@@ -27,16 +25,108 @@ $(document).ready(function () {
   $("#onePageNav").onePageNav({
     currentClass: "active",
     changeHash: false,
-    scrollSpeed: 500,
-    scrollThreshold: 0.2,
-    filter: "",
-    easing: "swing",
+    headerSelector: ".header",
+    offsetAdjustment: 10,
+    scrollThreshold: 0.3,
   });
   goTop();
+
+  // Smooth scroll for all on-page hash links (e.g., CTA buttons)
+  $('a[href^="#"]:not([data-bs-toggle])').on("click", function (e) {
+    var href = $(this).attr("href");
+    if (href && href !== "#" && $(href).length) {
+      var header = document.querySelector(".header");
+      var offset = header ? header.offsetHeight + 10 : 70;
+      var targetTop = $(href).offset().top - offset;
+      
+      e.preventDefault();
+      closeNav();
+      window.scrollTo({
+        top: Math.max(0, targetTop),
+        behavior: "smooth",
+      });
+    }
+  });
 
   $(".navbar-toggler").click(function () {
     $(".navbar-nav.collapsed-mobile").addClass("active");
   });
+
+  // Interactive Project Media Gallery & Video Controls
+  function initProjectMediaGallery() {
+    $(".project-showcase").each(function () {
+      var $showcase = $(this);
+      var $video = $showcase.find("video.main-video");
+      var $img = $showcase.find("img.main-img");
+      var $toggleBtn = $showcase.find(".media-btn-toggle");
+      var $muteBtn = $showcase.find(".media-btn-mute");
+
+      // Gallery thumbnail click
+      $showcase.find(".gallery-thumb").on("click", function () {
+        var $thumb = $(this);
+        var mediaType = $thumb.data("media-type");
+        var mediaSrc = $thumb.data("media-src");
+        var mediaPoster = $thumb.data("media-poster");
+        var mediaTitle = $thumb.data("title") || $thumb.attr("title");
+
+        $showcase.find(".gallery-thumb").removeClass("active");
+        $thumb.addClass("active");
+
+        if (mediaType === "video") {
+          $img.addClass("d-none");
+          $video.removeClass("d-none");
+          if ($video.length) {
+            var videoEl = $video[0];
+            if ($video.attr("src") !== mediaSrc) {
+              $video.attr("src", mediaSrc);
+              if (mediaPoster) $video.attr("poster", mediaPoster);
+            }
+            videoEl.play().catch(function () {});
+            $toggleBtn.find("i").removeClass("fa-play").addClass("fa-pause");
+          }
+          $showcase.find(".media-playback-controls").fadeIn(200);
+        } else {
+          if ($video.length) {
+            $video[0].pause();
+            $video.addClass("d-none");
+          }
+          $img.removeClass("d-none")
+            .attr("src", mediaSrc)
+            .attr("alt", mediaTitle || "Imperium Group Showcase");
+          $showcase.find(".media-playback-controls").fadeOut(200);
+        }
+      });
+
+      // Video Play/Pause toggle
+      $toggleBtn.on("click", function (e) {
+        e.stopPropagation();
+        if ($video.length) {
+          var videoEl = $video[0];
+          if (videoEl.paused) {
+            videoEl.play();
+            $toggleBtn.find("i").removeClass("fa-play").addClass("fa-pause");
+          } else {
+            videoEl.pause();
+            $toggleBtn.find("i").removeClass("fa-pause").addClass("fa-play");
+          }
+        }
+      });
+
+      // Video Mute/Unmute toggle
+      $muteBtn.on("click", function (e) {
+        e.stopPropagation();
+        if ($video.length) {
+          var videoEl = $video[0];
+          videoEl.muted = !videoEl.muted;
+          if (videoEl.muted) {
+            $muteBtn.find("i").removeClass("fa-volume-high").addClass("fa-volume-xmark");
+          } else {
+            $muteBtn.find("i").removeClass("fa-volume-xmark").addClass("fa-volume-high");
+          }
+        }
+      });
+    });
+  }
 
   $(".navbar-nav.collapsed-mobile .close__btn").click(closeNav);
   $(".navbar .overlay").click(closeNav);
@@ -77,4 +167,8 @@ $(document).ready(function () {
 
     observer.observe(statsSection);
   }
+
+  initProjectMediaGallery();
 });
+
+
